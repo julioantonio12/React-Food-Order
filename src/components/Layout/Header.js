9794678efc2e9122e5696fb7c1390d296ae514d8
Julio González Martín <jagonzalez@getinsoft.com>
2022-09-07 16:47:01 -0500

    Style Cart button

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,18 +1,19 @@
 import mealImg from "../../assets/meals.jpg";
 import classes from "../Styles/Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = (props) => {
   return (
     <>
       <header className={classes.header}>
         <h1>DeliMeal</h1>
-        <button>Cart</button>
+        <HeaderCartButton>Cart</HeaderCartButton>
       </header>
       <div className={classes["main-image"]}>
         <img src={mealImg} alt="Una mesa con comida deliciosa" />
       </div>
     </>
   );
-}
+};
 
 export default Header;
